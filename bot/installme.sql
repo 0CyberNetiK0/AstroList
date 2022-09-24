ad570a0407f4e5c169755cf216b840b12ed33805
Astra <60952407+AstraWrld@users.noreply.github.com>
2022-09-24 03:47:25 -0500

    Delete installme.sql

--- bot/installme.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `user_data` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `username` varchar(255) NOT NULL,
-    `discordId` varchar(255) NOT NULL,
-    PRIMARY KEY (`id`) USING BTREE
-);
